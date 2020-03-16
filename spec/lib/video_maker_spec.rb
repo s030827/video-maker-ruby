794812d31068d54f26f0d6927d65ba80a047ca8d
Marcus Almeida <s030827@gmail.com>
2020-03-15 23:45:39 -0300

    feat: add search term and prefix

---
+++ spec/lib/video_maker_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path('lib/video_maker')
+require 'tty-prompt'
+require 'byebug'
+
+describe VideoMaker do
+  describe "Capture search 'Banana'" do
+
+    subject(:movie) { VideoMaker.new }
+
+    it "Initialize'" do
+      expect(movie.data_structure['searchTerm']).to eq('Banana')
+      expect(movie.data_structure['prefix']).to eq('What is')
+    end
+  end
+end
