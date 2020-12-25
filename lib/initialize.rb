5665a2f14305e2bb2868759219a3053e7f081218
Marcus Almeida <s030827@gmail.com>
2020-12-25 18:04:57 -0300

    feat: download the first image of each sentence

--- lib/initialize.rb
+++ lib/initialize.rb
@@ -4,6 +4,9 @@ require 'algorithmia'
 require 'json'
 require 'ibm_watson/authenticators'
 require 'ibm_watson/natural_language_understanding_v1'
+require 'down'
+require 'fileutils'
+require 'byebug'
 
 require File.expand_path('robots/state.rb')
 require File.expand_path('robots/input.rb')
